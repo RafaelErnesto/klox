e641c98c6eea03609ea9b9ff41c9e3a2fcc721e6
Rafael Ernesto <rflernesto@gmail.com>
2023-08-10 20:20:30 -0400

    chore: scan single leximes

--- src/main/kotlin/Scanner.kt
+++ src/main/kotlin/Scanner.kt
@@ -18,4 +18,36 @@ class Scanner(private val source: String) {
     private fun isEnd(): Boolean =
         current >= source.length
 
+    private fun scanToken() {
+
+        when (advance()) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+            else -> Klox.error(line, "Unexpected character")
+        }
+    }
+
+    private fun advance(): Char {
+        val char = source[current]
+        current++
+        return char
+    }
+
+    private fun addToken(type: TokenType) {
+        addToken(type, null)
+    }
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
 }
